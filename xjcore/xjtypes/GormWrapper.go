71bff0ac68c6a286c53bd716b46a7e2950ae48c2
xjietop <107077584@qq.com>
2021-07-20 11:56:49 +0800

    增加GormWrapper

---
+++ xjcore/xjtypes/GormWrapper.go
@@ -0,0 +1,62 @@
+package xjtypes
+
+import (
+	"gorm.io/gorm"
+)
+
+type GormWrapper struct {
+	Where   string
+	Args    []interface{}
+	Cols    string
+	Orderby string
+	Current int
+	Size    int
+}
+
+func (this *GormWrapper) Eq(field string, val interface{}) *GormWrapper {
+	if this.Where != "" {
+		this.Where += " and "
+	}
+	this.Where += field + "=?"
+	if this.Args == nil || len(this.Args) == 0 {
+		this.Args = make([]interface{}, 0)
+	}
+	this.Args = append(this.Args, val)
+	return this
+}
+
+func (this *GormWrapper) In(field string, val interface{}) *GormWrapper {
+	if this.Where != "" {
+		this.Where += " and "
+	}
+	this.Where += field + "in(?)"
+	if this.Args == nil || len(this.Args) == 0 {
+		this.Args = make([]interface{}, 0)
+	}
+	this.Args = append(this.Args, val.([]interface{})...)
+	return this
+}
+
+func (this *GormWrapper) SetDb(db *gorm.DB) *gorm.DB {
+	if this.Where != "" {
+		if len(this.Args) > 0 {
+			db = db.Where(this.Where, this.Args...)
+		} else {
+			db = db.Where(this.Where)
+		}
+	}
+	if this.Cols != "" {
+		db = db.Select(this.Cols)
+	}
+	if this.Orderby != "" {
+		db = db.Order(this.Orderby)
+	}
+	start := (this.Current - 1) * this.Size
+	if start > 0 {
+		db = db.Offset(start)
+	}
+	if this.Size > 0 {
+		db = db.Limit(this.Size)
+	}
+	return db
+}
