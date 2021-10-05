5ed43734d737d49bb537293ac5d316bb453148ce
xjietop <107077584@qq.com>
2021-10-05 10:06:55 +0800

    IsEmail

--- xjcore/xjtypes/unicode.go
+++ xjcore/xjtypes/unicode.go
@@ -2,6 +2,7 @@ package xjtypes
 
 import "unicode"
 
+//是否数字
 func IsDigit(str string) bool {
 	for _, x := range []rune(str) {
 		if !unicode.IsDigit(x) {
@@ -9,4 +10,14 @@ func IsDigit(str string) bool {
 		}
 	}
 	return true
+}
+
+//是否电子邮箱地址
+func IsEmail(str string) bool {
+	for _, x := range []rune(str) {
+		if x == '@' {
+			return true
+		}
+	}
+	return false
 }
