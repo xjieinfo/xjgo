615a9e1ac2ba478692a87e4a417b8cbd9408b6b0
xjieinfo <63950846+xjieinfo@users.noreply.github.com>
2021-12-07 22:33:28 +0800

    Update context.go

--- xjhttp/context.go
+++ xjhttp/context.go
@@ -261,35 +261,35 @@ func (c *Context) String(code int, format string, values ...interface{}) {
 func (c *Context) JSON(code int, obj interface{}) {
 	c.Writer.WriteHeader(code)
 	j, _ := json.Marshal(obj)
-	fmt.Fprintf(c.Writer, string(j))
+	fmt.Fprint(c.Writer, string(j))
 }
 
 func (c *Context) Make(code int, rcode int, data interface{}, msg string) {
 	c.Writer.WriteHeader(code)
 	r := new(xjtypes.R).Make(rcode, data, msg)
 	j, _ := json.Marshal(r)
-	fmt.Fprintf(c.Writer, string(j))
+	fmt.Fprint(c.Writer, string(j))
 }
 
 func (c *Context) Success(code int, data interface{}) {
 	c.Writer.WriteHeader(code)
 	r := new(xjtypes.R).Success(data)
 	j, _ := json.Marshal(r)
-	fmt.Fprintf(c.Writer, string(j))
+	fmt.Fprint(c.Writer, string(j))
 }
 
 func (c *Context) Fail(code int, msg string) {
 	c.Writer.WriteHeader(code)
 	r := new(xjtypes.R).Fail(msg)
 	j, _ := json.Marshal(r)
-	fmt.Fprintf(c.Writer, string(j))
+	fmt.Fprint(c.Writer, string(j))
 }
 
 func (c *Context) Error(code int, err error) {
 	c.Writer.WriteHeader(code)
 	r := new(xjtypes.R).Fail(err.Error())
 	j, _ := json.Marshal(r)
-	fmt.Fprintf(c.Writer, string(j))
+	fmt.Fprint(c.Writer, string(j))
 }
 
 func (c *Context) Next() {
